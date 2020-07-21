c8ba7e8f88e05865a912a6a603e4b68646d37022
Joshua Palamuttam <joshpal97@gmail.com>
2020-07-21 01:19:50 -0400

    settings.json was modified with confidential info and added to the gitignore

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,14 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        client: "postgres",
+        host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {},
     },
